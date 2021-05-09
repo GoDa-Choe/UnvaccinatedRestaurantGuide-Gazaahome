5651090c552eb6522a62c44ec216ed02eb062f2d
goda <gyumyeong@khu.ac.kr>
2021-05-09 17:30:44 +0900

    게시글 태그 포함해서 작성하기 구현

--- forum/views.py
+++ forum/views.py
@@ -3,6 +3,7 @@ from forum.models import Post, Category, Tag
 from django.views.generic import ListView, DetailView, CreateView, UpdateView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.core.exceptions import PermissionDenied
+from django.utils.text import slugify
 
 
 class PostUpdate(LoginRequiredMixin, UpdateView):
@@ -10,12 +11,42 @@ class PostUpdate(LoginRequiredMixin, UpdateView):
     fields = ['title', 'content', 'head_image', 'category', 'tags']
     template_name = 'forum/post_update_form.html'
 
-
     def dispatch(self, request, *args, **kwargs):
         if request.user.is_authenticated and request.user == self.get_object().author:
             return super(PostUpdate, self).dispatch(request, *args, **kwargs)
         else:
             raise PermissionDenied
+
+    def get_context_data(self, **kwargs):
+        context = super(PostUpdate, self).get_context_data()
+        if self.object.tags.exists():
+            tags_str_list = []
+            for tag in self.object.tags.all():
+                tags_str_list.append(tag.name)
+            context['tags_str_default'] = '#' + '#'.join(tags_str_list)
+
+        return context
+
+    def form_valid(self, form):
+        response = super(PostUpdate, self).form_valid(form)
+        self.object.tags.clear()
+
+        tags_str = self.request.POST.get('tags_str')
+
+        if tags_str:
+            tags_str = tags_str.strip(' #')
+            tags_str = tags_str.replace(',', '#')
+            tags_list = tags_str.split('#')
+
+            for tag in tags_list:
+                tag = tag.strip()
+                tag, is_tag_created = Tag.objects.get_or_create(name=tag)
+                if is_tag_created:
+                    tag.slug = slugify(tag, allow_unicode=True)
+                    tag.save()
+                self.object.tags.add(tag)
+
+        return response
 
 
 class PostCreate(LoginRequiredMixin, CreateView):
@@ -26,7 +57,24 @@ class PostCreate(LoginRequiredMixin, CreateView):
         current_user = self.request.user
         if current_user.is_authenticated:
             form.instance.author = current_user
-            return super(PostCreate, self).form_valid(form)
+            response = super(PostCreate, self).form_valid(form)
+
+            tags_str = self.request.POST.get('tags_str')
+
+            if tags_str:
+                tags_str = tags_str.strip(' #')
+                tags_str = tags_str.replace(',', '#')
+                tags_list = tags_str.split('#')
+
+                for tag in tags_list:
+                    tag = tag.strip()
+                    tag, is_tag_created = Tag.objects.get_or_create(name=tag)
+                    if is_tag_created:
+                        tag.slug = slugify(tag, allow_unicode=True)
+                        tag.save()
+                    self.object.tags.add(tag)
+
+            return response
         else:
             return redirect('/forum/')
 
