dad500c40e404e69a5c6a48fdf17f4bd7192a90b
goda <gyumyeong@khu.ac.kr>
2021-05-10 04:48:46 +0900

    댓글 수정 기능 구현

--- forum/urls.py
+++ forum/urls.py
@@ -8,7 +8,8 @@ urlpatterns = [
     path('update_post/<int:pk>/', views.PostUpdate.as_view(), name='update'),
     path('create_post/', views.PostCreate.as_view(), name='create'),
 
-    path('<int:pk>/new_comment/', views.new_comment, name='comment'),
+    path('update_comment/<int:pk>/', views.CommentUpdate.as_view(), name='comment_update'),
+    path('<int:pk>/new_comment/', views.new_comment, name='comment_create'),
     path('<int:pk>/', views.PostDetail.as_view(), name='detail'),
     path('', views.PostList.as_view(), name='index')
 ]
