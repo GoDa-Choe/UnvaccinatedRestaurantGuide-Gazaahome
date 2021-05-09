6daa3e1eb1c5c056d9ce2af922a212ba1bc52a2b
goda <gyumyeong@khu.ac.kr>
2021-05-09 18:57:54 +0900

    게시글 마크다운 기능 추가

--- gazahome/urls.py
+++ gazahome/urls.py
@@ -21,6 +21,8 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('forum/', include('forum.urls')),
     path('admin/', admin.site.urls),
+
+    path('markdownx/', include('markdownx.urls')),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
