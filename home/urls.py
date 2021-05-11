bd970733585b5c66794e5d0544fc295720c36cd5
goda <gyumyeong@khu.ac.kr>
2021-05-11 10:22:52 +0900

    홈페이지 랜딩

--- home/urls.py
+++ home/urls.py
@@ -13,20 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from home import views
 
 urlpatterns = [
-    path('', include('home.urls')),
-
-    path('forum/', include('forum.urls')),
-
-    path('admin/', admin.site.urls),
-
-    path('accounts/', include('allauth.urls')),
-    path('markdownx/', include('markdownx.urls')),
+    path('', views.home, name='home'),
+    # path('about/', views.about, name='about'),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
