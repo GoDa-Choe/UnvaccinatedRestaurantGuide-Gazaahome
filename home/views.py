fea7fafd545f249ba76c4894fabf412edd8cb653
goda <gyumyeong@khu.ac.kr>
2021-05-11 13:26:16 +0900

    홈페이지 최근 게시글 구현

--- home/views.py
+++ home/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
+from forum.models import Post
 
 
 def home(request):
-    return render(request, 'home/index.html')
+    recent_posts = Post.objects.order_by('-pk')[:5]
+    context = {
+        'recent_posts': recent_posts,
+    }
+
+    return render(
+        request,
+        'home/index.html',
+        context
+    )
