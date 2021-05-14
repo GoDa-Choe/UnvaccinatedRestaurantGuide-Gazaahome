9ef622555294a2f171eb308a2401ca279c0b2296
goda <gyumyeong@khu.ac.kr>
2021-05-14 20:54:52 +0900

    좋아요 순 게시글 목록

--- home/views.py
+++ home/views.py
@@ -4,8 +4,15 @@ from forum.models import Post
 
 def home(request):
     recent_posts = Post.objects.order_by('-pk')[:5]
+    popular_posts = Post.objects.order_by("-hit_count_generic__hits", '-pk')[:5]
+
+    most_likes_posts = sorted(Post.objects.all(), key=lambda post: post.num_likes(), reverse=True)
+    most_likes_posts = most_likes_posts[:5]
+
     context = {
         'recent_posts': recent_posts,
+        'popular_posts': popular_posts,
+        'most_likes_posts': most_likes_posts,
     }
 
     return render(
