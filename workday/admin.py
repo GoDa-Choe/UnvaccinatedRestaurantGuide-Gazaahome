7d712c656cd69f3c0f04884715eaef3fbfc65ee5
goda <gyumyeong@khu.ac.kr>
2021-05-22 17:04:49 +0900

    실출근 계산기 모델 관리자 사이트 UI 개선

--- workday/admin.py
+++ workday/admin.py
@@ -1,6 +1,13 @@
 from django.contrib import admin
 from workday.models import Calculator, Leave, Dayoff
+from django.contrib import admin
 
-admin.site.register(Calculator)
+
+@admin.register(Calculator)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'created_at', 'updated_at', 'start_date', 'end_date', 'author']
+    list_display_links = ['id', 'name']
+
+
 admin.site.register(Leave)
 admin.site.register(Dayoff)
