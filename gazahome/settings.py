ee7e98e4a4b8d157aab649820b64d2b99441b80a
goda <gyumyeong@khu.ac.kr>
2021-08-02 15:05:46 +0900

    fake admin site 완성

--- gazahome/settings.py
+++ gazahome/settings.py
@@ -188,5 +188,3 @@ SECURE_SSL_REDIRECT = bool(os.environ.get('SECURE_SSL_REDIRECT', False))
 # Site Register Code
 GOOGLE_SITE_REGISTER_CODE = os.environ.get('GOOGLE_SITE_REGISTER_CODE', "")
 NAVER_SITE_REGISTER_CODE = os.environ.get('NAVER_SITE_REGISTER_CODE', "")
-
-ADMINS = [("goda", "goda.soft.studio@gmail.com"), ]
