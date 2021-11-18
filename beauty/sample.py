d212bd694cc0697d9af3e5862997e08f9478ede0
goda <gyumyeong@khu.ac.kr>
2021-11-18 21:14:42 +0900

    test

---
+++ beauty/sample.py
@@ -0,0 +1,52 @@
+import torch
+import torchvision
+
+from PIL import Image
+
+from pathlib import Path
+
+from facenet_pytorch import MTCNN
+
+DEVICE = torch.device('cpu')
+
+
+#####
+
+
+def evaluate(imgs, transform, model):
+    with torch.no_grad():
+        for img_path in imgs:
+            img = Image.open(Path('imgs') / img_path).convert('RGB')
+            img = transform(img)
+            img = img.unsqueeze(0)
+            score = model(img)
+            print(f"{score.item():.2f}", end=" ")
+
+
+if __name__ == "__main__":
+    transform = torchvision.transforms.Compose([
+        MTCNN(image_size=224, device=DEVICE),
+    ])
+
+    model = torchvision.models.resnet18()
+    fc_in = model.fc.in_features
+    model.fc = torch.nn.Linear(fc_in, 1)
+
+    WEIGHTS_PATH = "weights/resnet18_weights_60.pth"
+
+    model.load_state_dict(torch.load(WEIGHTS_PATH, map_location=DEVICE))
+    model.to(device=DEVICE)
+    model.eval()
+
+    imgs_good = ['img01.jpg', 'img02.jpg', 'img03.jpg', 'img04.jpg', 'img05.jpg', 'img06.jpg', 'img07.jpg']
+    imgs_bad = ['img12.jpg', 'img13.jpg', 'img14.jpg', 'img15.jpg']
+    imgs_mix = ['img18.jpg', 'img19.jpeg', 'img20.jpeg', 'img21.jpg', 'img22.jpeg']
+
+    evaluate(imgs_good, transform, model)
+    print()
+
+    evaluate(imgs_bad, transform, model)
+    print()
+
+    evaluate(imgs_mix, transform, model)
+    print()
--- beauty/views.py
+++ beauty/views.py
@@ -7,6 +7,15 @@ from django.views.generic import CreateView, FormView
 from beauty.models import Recode
 from beauty.forms import RecordForm, TestForm
 
+import torch
+import torchvision
+from PIL import Image
+import requests
+from facenet_pytorch import MTCNN
+import time
+from urllib import request
+from io import BytesIO
+
 
 class TestView(TemplateView):
     template_name = 'beauty/index.html'
@@ -27,11 +36,76 @@ class CreateRecord(CreateView):
     model = Recode
     form_class = RecordForm
     template_name = 'beauty/create_record.html'
-    success_url = '/thanks/'
 
     def form_valid(self, form):
-        current_user = self.request.user
-        form.instance.score = 0.0
-        obj = form.save()
+        start = time.time()
 
-        return render(self.request, template_name="beauty/index.html", context={"obj": obj})
+        form.instance.score = 0.0
+        form.instance.author = self.request.user
+        new_recode = form.save()
+
+        recode_save_time = time.time() - start
+
+        start = time.time()
+
+        model = self.get_resnet18()
+
+        model_load_time = time.time() - start
+
+        start = time.time()
+
+        model.eval()
+
+        with torch.no_grad():
+            face, face_detection_time = self.get_image(new_recode.face.url)
+
+            image_load_time = time.time() - start
+
+            start = time.time()
+
+            score = model(face)
+
+        new_recode.score = score.item()
+
+        predition_time = time.time() - start
+
+        context_data = {
+            "recode": new_recode,
+            "form_raw_image": self.request.FILES['face'],
+
+            "recode_save_time": recode_save_time,
+            "image_load_time": image_load_time - face_detection_time,
+            "face_detection_time": face_detection_time,
+            "model_load_time": model_load_time,
+            "predition_time": predition_time,
+            "total_time": recode_save_time + image_load_time + model_load_time + predition_time
+        }
+
+        return render(self.request, template_name="beauty/index.html",
+                      context=context_data)
+
+    def get_resnet18(self):
+        resnet18 = torchvision.models.resnet18()
+        fc_in = resnet18.fc.in_features
+        resnet18.fc = torch.nn.Linear(fc_in, 1)
+        resnet18.load_state_dict(
+            torch.load('beauty/static/beauty/weights/resnet18_weights_60.pth', map_location=torch.device('cpu')))
+
+        resnet18.to(device=torch.device('cpu'))
+
+        return resnet18
+
+    def get_image(self, url):
+        transform = torchvision.transforms.Compose([
+            MTCNN(image_size=224),
+        ])
+        # path = requests.get(url, stream=True).raw
+        # path = BytesIO(request.urlopen(url).read())
+        path = BytesIO(requests.get(url).content)
+
+        img = Image.open(path).convert('RGB')
+
+        start = time.time()
+        img = transform(img)
+        face_detection_time = time.time() - start
+        return img.unsqueeze(dim=0), face_detection_time
