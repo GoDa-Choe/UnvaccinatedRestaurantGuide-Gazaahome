e9125e13acbe8d43232d57cadd7132795a98908c
goda <gyumyeong@khu.ac.kr>
2021-11-19 18:05:25 +0900

    test

--- beauty/views.py
+++ beauty/views.py
@@ -18,9 +18,10 @@ class PredictView(FormView):
     template_name = 'beauty/prediction.html'
 
     def form_valid(self, form):
-        predictor = get_resnet18()
         form.instance.author = self.request.user
         face = form.save()
+
+        predictor = get_resnet18()
 
         with torch.no_grad():
             image = get_image(face.face.url)
