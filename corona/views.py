c2defd57b9d4ddfb2b7bdf199c754a557293c7d3
goda <gyumyeong@khu.ac.kr>
2021-12-29 00:07:24 +0900

    test 2

--- corona/views.py
+++ corona/views.py
@@ -167,11 +167,11 @@ class MapView(RestaurantList):
                 'longitude': restaurant.longitude,
                 'url': str(restaurant.url),
                 'category': restaurant.category.name,
-                'tags': [tag.name for tag in restaurant.tags.all()],
+                # 'tags': [tag.name for tag in restaurant.tags.all()],
                 'unvaccinated_pass': restaurant.unvaccinated_pass.type,
-                'num_likes': restaurant.likes.count(),
-                'num_dislikes': restaurant.num_dislikes(),
-                'num_comments': restaurant.num_comments(),
+                # 'num_likes': restaurant.likes.count(),
+                # 'num_dislikes': restaurant.num_dislikes(),
+                # 'num_comments': restaurant.num_comments(),
                 'hits': restaurant.hit_count.hits,
             }
             restaurant_list.append(data)
