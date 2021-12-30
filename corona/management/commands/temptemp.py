30563c12010e89ecf6384bad8ed9e668cc3c14c4
goda <gyumyeong@khu.ac.kr>
2021-12-31 00:12:01 +0900

    tesa

--- corona/management/commands/temptemp.py
+++ corona/management/commands/temptemp.py
@@ -7,12 +7,13 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         try:
-            for restaurant in Restaurant.objects.iterator():
+            for restaurant in Restaurant.objects.all():
                 if restaurant.region == "세종특별자치시":
-                    restaurant.update(region="세종")
+                    restaurant.region = "세종"
                     restaurant.save()
                 elif restaurant.region == "제주특별자치도":
-                    restaurant.update(region="제주")
+                    restaurant.region = "제주"
+                    restaurant.save()
 
         except CommandError as error:
             self.stdout.write(self.style.ERROR(f'CommandError was raised. {error}'))
