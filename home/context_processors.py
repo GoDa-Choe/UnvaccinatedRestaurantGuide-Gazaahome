b4144ac1fe527d60f0a6ef2efa59b4cc1f3f170e
goda <gyumyeong@khu.ac.kr>
2022-01-04 00:22:39 +0900

    긴급 공지

--- home/context_processors.py
+++ home/context_processors.py
@@ -1,17 +1,26 @@
 from home.models import Corona
+from corona.models import Post
+from django.contrib.auth.models import User
+import os
 
 
 def get_corona_decided_count(request):
     today_yesterday = Corona.objects.order_by("-state_date")[:2]
+    id = os.environ.get('NOTIFICATION_ID', None)
+
+    notification = Post.objects.get(pk=8) if id is not None else None
+
     if len(today_yesterday) == 2:
         today, yesterday = today_yesterday
 
         return {
             "decided_count": today.decided_count,
             "today_decided_count": today.decided_count - yesterday.decided_count,
+            "notification": notification
         }
 
     return {
         "decided_count": 0,
         "today_decided_count": 0,
+        "notification": notification
     }
